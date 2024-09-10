a5b7696f0bb1e4fa2c1d149db386bc942539dd60
Sthepen_EA <sthepen.espinoza29@gmail.com>
2024-09-10 12:30:03 -0500

    base project v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './shared/components/layouts/header/header.component';
+import { FooterComponent } from './shared/components/layouts/footer/footer.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+``;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    LandingpageComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ctp-app';
